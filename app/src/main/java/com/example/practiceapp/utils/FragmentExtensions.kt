7113bc18731980ab21453d83458e0864a585fc2c
jamessavery <jamesS275@hotmail.co.uk>
2023-04-28 18:18:45 +0100

    Attempted to add Moshi - removed now

--- app/src/main/java/com/example/practiceapp/utils/FragmentExtensions.kt
+++ app/src/main/java/com/example/practiceapp/utils/FragmentExtensions.kt
@@ -5,9 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
-//Todo 1) Adapt from OneBottomSheet
-// 2) Steal the callback logic too addOnStateChangedCallBack()
-// 3) This should be bottom sheet extension and could handle swiping too
+
 fun <T : Fragment> showBottomSheet(context: Context, view: View) {
     val dialog = BottomSheetDialog(context)
     //val view = layoutInflater.
